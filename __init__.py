6edb0562a936175e978697e762e2d20564b3e52d
Dima Kogan <dkogan@debian.org>
2023-09-28 12:09:16 -0700

    "rostopic echo" without -p no longer outputs arrays of bytes
    This patch is wrong, and far too heavy-handed. But at least we no longer spam
    the terminal

--- __init__.py
+++ __init__.py
@@ -877,11 +877,11 @@ class CallbackEcho(object):
 
     def custom_strify_message(self, val, indent='', time_offset=None, current_time=None, field_filter=None,
                               type_information=None, fixed_numeric_width=None, value_transform=None):
-        # ensure to print uint8[] as array of numbers instead of string
-        if type_information and type_information.startswith('uint8['):
-            val = [ord(x) for x in val]
-        if value_transform is not None:
-            val = value_transform(val, type_information)
+
+        # Don't print split bytes I can also patch value_transform, since that's
+        # what I get with "rostopic echo" without -p
+        val = '....'
+
         return genpy.message.strify_message(val, indent=indent, time_offset=time_offset, current_time=current_time, field_filter=field_filter, fixed_numeric_width=fixed_numeric_width)
 
     def callback(self, data, callback_args, current_time=None):
