d099e8b99f11403d25c27de297b6a5226beeba49
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2024-09-16 19:40:09 -0700

    added show-lidar-scans.py

---
+++ show_lidar_scans.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+r'''Display the LIDAR scans from a rosbag
+
+SYNOPSIS
+
+  $ ./show_lidar_scans.py     \
+      --period 2              \
+      --set "view 60,30,2"    \
+      --set "xrange [-20:20]" \
+      --set "yrange [-20:20]" \
+      --set "zrange [-2:2]"   \
+      'camera-lidar*.bag'     \
+      /lidar/vl_points_1
+    [A plot pops up showing the scans from the bags, updating every 2 sec]
+
+This is a display tool to show the LIDAR scans one at a time. It's a very poor
+man's rviz display, without requiring any ros tools to be installed
+
+'''
+
+
+import sys
+import argparse
+import re
+import os
+
+
+def parse_args():
+
+    parser = \
+        argparse.ArgumentParser(description = __doc__,
+                                formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--period',
+                        type=float,
+                        default = 0,
+                        help = '''How much time to wait between moving to the
+                        next bag; if <= 0 (the default), we wait until each
+                        window is manually closed''')
+    parser.add_argument('--set',
+                        type=str,
+                        action='append',
+                        help='''Extra 'set' directives to gnuplotlib. Can be given multiple times''')
+    parser.add_argument('--unset',
+                        type=str,
+                        action='append',
+                        help='''Extra 'unset' directives to gnuplotlib. Can be given multiple times''')
+
+    parser.add_argument('lidar-topic',
+                        type=str,
+                        help = '''The LIDAR topic we're looking at''')
+
+    parser.add_argument('bags',
+                        type=str,
+                        nargs='+',
+                        help = '''Glob(s) for the rosbags that contain the lidar
+                        data. Each of these will be sorted alphanumerically''')
+
+
+    args = parser.parse_args()
+    return args
+
+
+args = parse_args()
+
+import bag_interface
+import glob
+import numpy as np
+import numpysane as nps
+import gnuplotlib as gp
+import time
+
+
+lidar_topic = getattr(args, 'lidar-topic')
+
+for bag_glob in args.bags:
+    bags = sorted(glob.glob(bag_glob))
+    if len(bags) == 0:
+        print(f"No files matched the glob '{bag_glob}'", file=sys.stderr)
+        sys.exit(1)
+
+    for bag in bags:
+        p = next(bag_interface.bag_messages_generator(bag, (lidar_topic,) ))['array']
+
+        xyz       = p['xyz']
+        intensity = p['intensity']
+        gp.plot(xyz[:,0], xyz[:,1], xyz[:,2], intensity,
+                tuplesize = 4,
+                _with  = 'dots',
+                square = True,
+                _3d    = True,
+                _set   = args.set,
+                _unset = args.unset,
+                title  = f"{bag=} {lidar_topic=}",
+                xlabel = 'x',
+                ylabel = 'y',
+                wait   = args.period <= 0)
+
+        if args.period > 0:
+            time.sleep(args.period)
