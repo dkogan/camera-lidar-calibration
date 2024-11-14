ee33f3fd478c5125918ec7c3ff4f8533a4cdd122
Dima Kogan <dima@secretsauce.net>
2024-11-13 16:15:40 -0800

    Added Python clc.calibrate() to wrap _clc.calibrate()
    This provides a rosbag interface

--- clc.py
+++ clc.py
@@ -35,4 +35,31 @@ def lidar_segmentation(*,
               plane_p = plane_pn[:,:3],
               plane_n = plane_pn[:,3:] )
 
+
+def calibrate(*,
+              bags, lidar_topics):
+
+    def lidar_points(bag, lidar_topic):
+        array = next(bag_interface.bag_messages_generator(bag, (lidar_topic,) ))['array']
+
+        points = array['xyz']
+        rings  = array['ring']
+
+        return (points,rings)
+
+    def lidar_points_all_topics(bag):
+        return tuple( lidar_points(bag, lidar_topic) for lidar_topic in lidar_topics )
+
+    def sensor_snapshot(bag):
+        if not os.path.exists(bag):
+            raise Exception(f"Bag path '{bag}' does not exist")
+        return (None, lidar_points_all_topics(bag))
+
+    for i,bag in enumerate(bags):
+        print(f"Bag {i: 3d} {bag}")
+    for i,topic in enumerate(lidar_topics):
+        print(f"Topic {i: 2d} {topic}")
+
+    return _clc.calibrate( tuple(sensor_snapshot(bag) for bag in bags) )
+
 lidar_segmentation_default_context = _clc.lidar_segmentation_default_context
