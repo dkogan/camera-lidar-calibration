3cc1effd01b090ac4d26af74b49dd4b3e33bd14b
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2024-12-19 19:07:14 -0800

    show-bag.py can display lidar or camera data

--- show-bag.py
+++ show-bag.py
@@ -5,17 +5,40 @@ r'''Display the LIDAR scans from a rosbag
 SYNOPSIS
 
   $ ./show-bag.py             \
+      'camera-lidar*.bag'
+
+  Bag 'camera-lidar-0.bag':
+    /points0
+    /points1
+  Bag 'camera-lidar-1.bag':
+    /points0
+    /points1
+    /image0
+  ....
+
+
+  $ ./show-bag.py             \
       --period 2              \
       --set "view 60,30,2"    \
       --set "xrange [-20:20]" \
       --set "yrange [-20:20]" \
       --set "zrange [-2:2]"   \
-      /lidar/vl_points_1      \
+      --topic /points0 \
       'camera-lidar*.bag'
-    [A plot pops up showing the scans from the bags, updating every 2 sec]
 
-This is a display tool to show the LIDAR scans one at a time. It's a very poor
-man's rviz display, without requiring any ros tools to be installed
+    [A plot pops up showing the LIDAR scans from the bags, updating every 2 sec]
+
+
+  $ ./show-bag.py     \
+      --topic /image0 \
+      'camera-lidar*.bag'
+
+    [A plot pops up showing the images in the bags. Updated with the next bag
+    when the user closes the plot]
+
+This is a display tool to show the contents of a ros bag. This is roughly
+similar to the "rosbag" and "ros2 bag" and "rviz" tools, but uses the "rosbags"
+Python package, and does NOT require ROS to be installed
 
 '''
 
@@ -34,14 +57,16 @@ def parse_args():
 
     parser.add_argument('--maxrange',
                         type=float,
-                        help = '''If given, cut off the points at this range''')
+                        help = '''Applies to LIDAR data. If given, cut off the
+                        points at this range''')
     parser.add_argument('--ring',
                         type=int,
-                        help = '''If given, show ONLY data from this ring.
-                        Otherwise, display all of them''')
+                        help = '''Applies to LIDAR data. If given, show ONLY
+                        data from this ring. Otherwise, display all of them''')
     parser.add_argument('--xy',
                         action='store_true',
-                        help = '''If given, I make a 2D plot, ignoring the z axis''')
+                        help = '''Applies to LIDAR data. If given, I make a 2D
+                        plot, ignoring the z axis''')
     parser.add_argument('--period',
                         type=float,
                         default = 0,
@@ -59,8 +84,9 @@ def parse_args():
 
     parser.add_argument('--topic',
                         type=str,
-                        help = '''The topic we're visualizing. If omitted, we
-                        report the topics present in the bag, and we exit''')
+                        help = '''The topic we're visualizing. Can select LIDAR
+                        or camera data. If omitted, we report the topics present
+                        in the bag, and we exit''')
 
     parser.add_argument('bags',
                         type=str,
@@ -106,13 +132,14 @@ import gnuplotlib as gp
 import time
 
 
-for bag in bags():
-    try:
-        p = next(bag_interface.messages(bag, (topic,) ))['array']
-    except StopIteration:
-        print(f"No messages with {topic=} in {bag=}. Continuing to next bag, if any",
-              file = sys.stderr)
-        continue
+def show_lidar(bag, p):
+    kwargs = dict( _set   = args.set,
+                   _unset = args.unset)
+
+    gp.add_plot_option(kwargs,
+                       title = bag,
+                       overwrite = False)
+
 
     xyz       = p['xyz']
     intensity = p['intensity']
@@ -137,12 +164,77 @@ for bag in bags():
             _with  = 'dots palette',
             square = True,
             _3d    = not args.xy,
-            _set   = args.set,
-            _unset = args.unset,
-            title  = f"{bag=} {topic=}",
+            title  = f"{bag=} {args.topic=}",
             xlabel = 'x',
             ylabel = 'y',
-            wait   = args.period <= 0)
+            wait   = args.period <= 0,
+            **kwargs)
 
     if args.period > 0:
         time.sleep(args.period)
+
+
+
+def show_image(bag, p):
+    kwargs = dict( _set   = args.set,
+                   _unset = args.unset)
+
+    gp.add_plot_option(kwargs,
+                       title = bag,
+                       overwrite = False)
+
+    gp.add_plot_option(kwargs,
+                       set = ('xrange [:] noextend',
+                              'yrange [:] noextend reverse',),
+                       unset = 'colorbox')
+
+    if p.ndim == 2:
+        # grayscale
+        gp.add_plot_option(kwargs,
+                           set = 'palette gray')
+
+        gp.plot(p,
+                _with     = 'image',
+                tuplesize = 3,
+                square    = True,
+                wait      = args.period <= 0,
+                **kwargs)
+    else:
+        # color
+        gp.plot(p[...,2], p[...,1], p[...,0],
+                _with     = 'rgbimage',
+                tuplesize = 5,
+                square    = True,
+                wait      = args.period <= 0,
+                **kwargs)
+
+    if args.period > 0:
+        time.sleep(args.period)
+
+
+
+for bag in bags():
+    try:
+        p = next(bag_interface.messages(bag, (args.topic,) ))['array']
+    except StopIteration:
+        print(f"No messages with {args.topic=} in {bag=}. Continuing to next bag, if any",
+              file = sys.stderr)
+        continue
+
+
+    def has_xyz(p):
+        try:
+            if 'xyz' in p.dtype.names:
+                return True
+        except:
+            return False
+
+    if has_xyz(p):
+        show_lidar(bag, p)
+    elif p.dtype == np.uint8 and \
+         (p.ndim == 2 or (p.ndim==3 and p.shape[-1] == 3)):
+        show_image(bag, p)
+    else:
+        print(f"Cannot interpret message from {args.topic}",
+              file=sys.stderr)
+        sys.exit(1)
