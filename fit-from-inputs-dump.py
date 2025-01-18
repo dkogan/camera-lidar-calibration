8b8bca796e95c9ab9cdac13ecb36cf9e23ef0074
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2025-01-17 16:53:16 -0800

    added fit-from-inputs-dump.py

---
+++ fit-from-inputs-dump.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+r'''Calibrate from a binary input dump
+
+For debugging
+
+'''
+
+
+import sys
+import argparse
+import re
+import os
+
+def parse_args():
+
+    parser = \
+        argparse.ArgumentParser(description = __doc__,
+                                formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--inject-noise',
+                        action='store_true',
+                        help = '''If given, we add expected noise to the
+                        observations. If --inject-noise and no --fit-seed then I
+                        will fit() from the previous fit() result, NOT from the
+                        previous fit_seed() result.''')
+    parser.add_argument('--fit-seed',
+                        action='store_true',
+                        help = '''If given, we fit_seed() and then fit(). By
+                        default (no --fit-seed), we only call fit(): from the
+                        previous fit_seed() result if no --inject-noise or from
+                        the previous fit() result if --inject-noise''')
+    parser.add_argument('context',
+                        help = '''.pickle file from fit.py --dump''')
+    args = parser.parse_args()
+
+    return args
+
+
+args = parse_args()
+
+import pickle
+import numpy as np
+import numpysane as nps
+import mrcal
+import clc
+
+import testutils
+
+with open(args.context, "rb") as f:
+    context = pickle.load(f)
+
+result = clc.fit_from_inputs_dump(context['result']['inputs_dump'],
+                                  do_inject_noise = args.inject_noise,
+                                  do_fit_seed     = args.fit_seed)
