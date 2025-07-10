3684d8a0ba530602a7a68235eff602376b46db03
Dima Kogan <dima@secretsauce.net>
2025-07-10 13:39:59 -0700

    Error checking

--- clc/wrappers.py
+++ clc/wrappers.py
@@ -590,6 +590,16 @@ A dict describing the result. The items are:
                                  max_time_spread_s    = max_time_spread_s,
                                  exclude_time_periods = exclude_time_periods,
                                  verbose              = verbose)
+
+    Ncameras_models = len(kwargs['models'])
+    Ncameras_topics = len(itopics_camera)
+    if Ncameras_models != Ncameras_topics:
+        raise Exception(f"I havee {Ncameras_topics=} != {Ncameras_models=}")
+
+    if len(itopics_lidar) == 0:
+        raise Exception("No lidar topics given")
+
+
     result = _clc.calibrate( snapshots,
                              verbose = verbose,
                              **kwargs)
